78cd727375baa49e966be7a90ea9c5a10a7ee573
seeeeeeong <lsinsung@gmail.com>
2025-01-20 19:14:04 +0900

    20250120

--- PycharmProjects/dingcorithm/dingcorithm/1st_week/01_06_find_list_under_number.py
+++ PycharmProjects/dingcorithm/dingcorithm/1st_week/01_06_find_list_under_number.py
@@ -3,8 +3,9 @@ input = 20
 
 def find_prime_list_under_number(number):
     answer = []
+
     for i in range(2, number + 1):
-        for j in range(2, int(i ** 0.5) + 1):
+        for j in range(2, int(i**0.5) + 1):
             if i % j == 0:
                 break
         else:
@@ -12,10 +13,5 @@ def find_prime_list_under_number(number):
 
     return answer
 
-
 result = find_prime_list_under_number(input)
 print(result)
-
-# 20 이하의 소수
-# for i in range number + 1
-#   for j in range(i, int(i**0.5) + 1)
