fb4817f2b16fd500004ef09e338363d5262592e3
seeeeeeong <lsinsung@gmail.com>
2025-01-24 18:42:59 +0900

    20250124

--- PycharmProjects/dingcorithm/dingcorithm/1st_week/01_06_find_list_under_number.py
+++ PycharmProjects/dingcorithm/dingcorithm/1st_week/01_06_find_list_under_number.py
@@ -3,15 +3,15 @@ input = 20
 
 def find_prime_list_under_number(number):
     answer = []
-
     for i in range(2, number + 1):
-        for j in range(2, int(i**0.5) + 1):
+        for j in range(2, int(i ** 0.5) + 1):
             if i % j == 0:
                 break
         else:
             answer.append(i)
 
     return answer
+
 
 result = find_prime_list_under_number(input)
 print(result)
